b92efea78c3630b577b388b451c55ddad4b3dcca
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-04-22 15:11:59 -0300

    Aula 01

---
+++ src/aula01/aula01.ts
@@ -0,0 +1,13 @@
+/*
+Aula 01:
+
+Instalação do Node.js.
+Link: https://nodejs.org/en
+
+Instalação do TypeScript.
+Global: npm install -g typescript
+No projeto: npm install typescript --save-dev
+
+Extensão para o VS Code.
+Extensão: JavaScript and TypeScript Nightly (Microsoft)
+*/
